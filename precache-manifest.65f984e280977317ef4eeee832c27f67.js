13d4a17659a518dc84831f261d7f09dcd077b0ba
jojocepeda <cepedajojo20@gmail.com>
2021-06-29 16:07:48 -0300

    Updates

---
+++ precache-manifest.65f984e280977317ef4eeee832c27f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cc98e03f561cfc58c56",
+    "url": "/social-network-dapp/static/css/main.a2849921.chunk.css"
+  },
+  {
+    "revision": "9cc98e03f561cfc58c56",
+    "url": "/social-network-dapp/static/js/main.9cc98e03.chunk.js"
+  },
+  {
+    "revision": "2dced35e6298d1d224b9",
+    "url": "/social-network-dapp/static/css/1.4d9bff51.chunk.css"
+  },
+  {
+    "revision": "2dced35e6298d1d224b9",
+    "url": "/social-network-dapp/static/js/1.2dced35e.chunk.js"
+  },
+  {
+    "revision": "e84404e51d3d1c792318",
+    "url": "/social-network-dapp/static/js/runtime~main.e84404e5.js"
+  },
+  {
+    "revision": "7da6a40616009fee9b66a3a00757f84b",
+    "url": "/social-network-dapp/index.html"
+  }
+];
